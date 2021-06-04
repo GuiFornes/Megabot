b526684e2e18f496376f0ee79090713427bddf7d
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2021-06-04 17:50:58 +0200

    pb sur les thetas

--- upg_kinetic.py
+++ upg_kinetic.py
@@ -1,29 +1,37 @@
+# L'ensemble des distances sont exprimées en mm : segments de patte et élongations des vérins
+
 import numpy as np
 from numpy.linalg import inv
 from numpy import dot
 
 import kinetic as k
 
-x_A = - k.LEG_PARTS_LENGTHS['ao']
+x_A = - k.LEGS[k.FR]['lengths']['ao']
 y_A = 0
-x_B = k.LEG_PARTS_LENGTHS['bo']
+x_B = k.LEGS[k.FR]['lengths']['bo']
 y_B = 0
 
-AB = k.LEG_PARTS_LENGTHS['ao'] + k.LEG_PARTS_LENGTHS['bo']
-AC = np.sqrt((AB + k.LEG_PARTS_LENGTHS['bcx'])**2 + k.LEG_PARTS_LENGTHS['bcy']**2)
-BC = np.sqrt(k.LEG_PARTS_LENGTHS['bcx']**2 + k.LEG_PARTS_LENGTHS['bcy']**2)
-AE = k.LEG_PARTS_LENGTHS['ae']
-AD = AE - k.LEG_PARTS_LENGTHS['de']
-BF = k.LEG_PARTS_LENGTHS['bf']
-FH = k.LEG_PARTS_LENGTHS['fh']
+AB = k.LEGS[k.FR]['lengths']['ao'] + k.LEGS[k.FR]['lengths']['bo']
+AC = np.sqrt((AB + k.LEGS[k.FR]['lengths']['bcx'])**2 + k.LEGS[k.FR]['lengths']['bcy']**2)
+BC = np.sqrt(k.LEGS[k.FR]['lengths']['bcx']**2 + k.LEGS[k.FR]['lengths']['bcy']**2)
+AE = k.LEGS[k.FR]['lengths']['ae']
+AD = AE - k.LEGS[k.FR]['lengths']['de']
+BF = k.LEGS[k.FR]['lengths']['bf']
+FH = k.LEGS[k.FR]['lengths']['fh']
 BH = BF - FH
-FG = k.LEG_PARTS_LENGTHS['fg']
-EF = k.LEG_PARTS_LENGTHS['ef']
+FG = k.LEGS[k.FR]['lengths']['fg']
+EF = k.LEGS[k.FR]['lengths']['ef']
 EG = EF + FG
-GI = k.LEG_PARTS_LENGTHS['gi']
-GJ = k.LEG_PARTS_LENGTHS['gj']
+GI = k.LEGS[k.FR]['lengths']['gi']
+GJ = k.LEGS[k.FR]['lengths']['gj']
 
+KO = k.LEGS[k.FR]['lengths']['yaw_c']
+LM = k.LEGS[k.FR]['lengths']['yaw_b']
+MO = k.LEGS[k.FR]['lengths']['yaw_a']
+LO = np.sqrt(LM**2 + MO**2)
+beta = np.arctan(LM/MO)
 
+V = 460, 565, 500
 
 '''
 Retourne la Jacobienne correspondant au modèle cinématique indirect
@@ -44,7 +52,7 @@ def gen_jacob(pts, v1, v2):
     [2*(x_D - x_A), 2*(y_D - y_A)], 
     [2*(x_D - x_C), 2*(y_D - y_C)]])
   B = np.array([0, 2*v1])
-  M_D = dot(inv(A), B)
+  M_D = inv(A) @ B
 
   M_E = (AE/AD) * M_D
 
@@ -54,7 +62,7 @@ def gen_jacob(pts, v1, v2):
   B = np.array([
     [2*(x_F - x_E), 2*(y_F - y_E)], 
     [0, 0]])
-  M_F = dot(dot(inv(A), B), M_E)
+  M_F = (inv(A) @ B) @ M_E
 
   M_G = (EG/EF) * M_F
 
@@ -70,8 +78,8 @@ def gen_jacob(pts, v1, v2):
     [0, 0],
     [2*(x_I - x_H), 2*(y_I - y_H)]])
   D = np.array([0, 2*v2])
-  V1 = dot(inv(A),(dot(B, M_G) + dot(C, M_H)))
-  V2 = dot(inv(A), D) 
+  V1 = inv(A) @ (B @ M_G + C @ M_H)
+  V2 = inv(A) @ D 
   M_I = np.array([[V1[0], V2[0]], [V1[1], V2[1]]])
 
   Jacob = inv((GJ/GI) * M_I)
@@ -96,47 +104,56 @@ Fonction auxiliaire de move_xyz
 Retourne l'élongation de v3 en fonction de l'angle de la patte au chassis
 '''
 def angle_to_v3(angle):
-  return np.sqrt(LO**2 + KO**2 - 2*LO*KO*np.cos(angle))
+  #print(angle)
+  return np.sqrt(LO**2 + KO**2 - 2*LO*KO*np.cos(angle - beta))
 
 '''
 Fonction auxiliaire de move_xyz
 Retourne l'angle de la patte au chassis en fonction de l'élongation de v3
 '''
 def v3_to_angle(v3):
-  return np.arccos((KO**2 + LO**2 - v3**2)/(2*KO*LO))
+  #print(v3)
+  return np.arccos((KO**2 + LO**2 - v3**2)/(2*KO*LO)) + beta
 
 '''
 Retourne la liste des élongations successives des verrins permettant de placer le bout de la patte en (x,y,z)
 '''
 def move_xyz(x, y, z, dstep, eps):
   L = []
+  c = 0
   X = np.sqrt(x**2 + y**2)
   v1, v2, v3 = get_ver()
-  pts = k.get_leg_points_V1_V2(v1, v2, k.LEG_PARTS_LENGTHS)
-  cur_X, cur_Y = pts['J']
+  pts = k.get_leg_points_V1_V2(v1/1000, v2/1000, k.LEGS[k.FR]['lengths'])
+  cur_X, cur_Y = pts['J'][0]*1000, pts['J'][1]*1000
   cur_angle = v3_to_angle(v3)
-  cur_x, cur_y = cur_X * np.cos(cur_angle), cur_X * np.sin(cur_angle)
+  cur_x, cur_y = cur_X * np.cos(cur_angle), - cur_X * np.sin(cur_angle)
   dist = distance(x - cur_x, y - cur_y, z - cur_Y)
-  while dist > eps:
+  while dist > eps and c < 1000:
+    c += 1
     # on détermine dx, dy, dz tq conforme à dstep
-    theta1 = np.arccos((x - cur_x)/(X - cur_X))
-    theta2 = np.arccos((X - cur_X)/(z - cur_Y))
+    theta1 = np.arctan((y - cur_y)/(x - cur_x))
+    theta2 = np.arctan((z - cur_Y)/(X - cur_X))
+    print(theta1, theta2)
     dx, dy, dz = deltas(theta1, theta2, dstep)
-
+    #print(dx, dy, dz)
     # on calcule les nouveaux v1, v2, v3 (et on les ajoute à L)
-    dv3 = angle_to_v3(np.tan(dy/dx))
+    new_v3 = angle_to_v3(np.arctan((cur_y + dy)/(cur_x + dx))) 
     dX = np.sqrt(dx**2 + dy**2)
-    deltaXY = np.array([dX, dz])
-    Jacob = gen_jacob(pts, v1, v2)
-    dv1v2 = np.dot(Jacob, deltaXY)
-    v1 += dv1v2[0]
-    v2 += dv1v2[1]
-    v3 += dv3
+    #print(dz)
+    deltaXY = np.array([dX/1000, dz/1000])
+    Jacob = gen_jacob(pts, v1/1000, v2/1000)
+    #print(Jacob)
+    dv1v2 = Jacob @ deltaXY
+    #print(dv1v2)
+    v1 += dv1v2[0]*1000
+    v2 += dv1v2[1]*1000
+    v3 = new_v3
+    #print(v1, v2, v3)
     L.append((v1, v2, v3))
 
     # on recalcule les nouvelles positions courantes et la distance à l'objectif
-    pts = k.get_leg_points_V1_V2(v1, v2, k.LEG_PARTS_LENGTHS)
-    cur_X, cur_Y = pts['J']
+    pts = k.get_leg_points_V1_V2(v1/1000, v2/1000, k.LEGS[k.FR]['lengths'])
+    cur_X, cur_Y = pts['J'][0]*1000, pts['J'][1]*1000
     cur_angle = v3_to_angle(v3)
     cur_x, cur_y = cur_X * np.cos(cur_angle), cur_X * np.sin(cur_angle)
     dist = distance(x - cur_x, y - cur_y, z - cur_Y)
@@ -147,12 +164,14 @@ def move_xyz(x, y, z, dstep, eps):
 '''
 Retourne les positions x, y, z du bout de la patte en fonctions des v1, v2, v3
 '''
-def direct(v1 ,v2, v3):
-  X, Y = k.get_leg_points_V1_V2(v1, v2, k.LEG_PARTS_LENGTHS)['J']
+def direct_xyz(v1 ,v2, v3):
+  X, Y = k.get_leg_points_V1_V2(v1/1000, v2/1000, k.LEGS[k.FR]['lengths'])['J']
+  #print (X, Y)
   alpha = v3_to_angle(v3)
-  x = X * np.cos(alpha)
-  y = Y * np.sin(alpha)
-  return x, y, Y 
+  x = X * np.cos(alpha) * 1000
+  y = -X * np.sin(alpha) * 1000
+  z = Y * 1000
+  return x, y, z
 
 
 
@@ -167,7 +186,12 @@ Retourne les positions des verrins
 '''
 def get_ver():
   #return 485, 545, 515
-  return (controlers[l].la[1]['position']+450.0)/1000.0, (controlers[l].la[0]['position']+450.0)/1000.0, (controlers[l].la[2]['position']+450.0)/1000.0
+  #return (controlers[l].la[1]['position']+450.0)/1000.0, (controlers[l].la[0]['position']+450.0)/1000.0, (controlers[l].la[2]['position']+450.0)/1000.0
+  return V
+
+def set_ver(v1, v2, v3):
+  global V
+  V = v1, v2, v3
 
 
 
--- upg_tests.py
+++ upg_tests.py
@@ -7,117 +7,140 @@ import time
 
 import kinetic as k
 from upg_kinetic import *
-
 '''
   Retourne l'erreur relative en x et en y de l'application de la Jacobienne du modèle indirect
   Prend en argument l'élongation des vérins et la distance de déplacement selon x et y en m
 '''
+
+
 def test_jacob(v1, v2, dstep_x, dstep_y):
-   
-  pts = k.get_leg_points_V1_V2(v1, v2, k.LEG_PARTS_LENGTHS)
-  x, y = pts['J']
-  Jacob = gen_jacob(pts, v1, v2)
-  Dpos = np.array([dstep_x, dstep_y])
-  DV = np.dot(Jacob, Dpos)
-  
-  pts = k.get_leg_points_V1_V2(v1 + DV[0], v2 + DV[1], k.LEG_PARTS_LENGTHS)
-  print("position initiale : ", x, y)
-  
-  new_x, new_y = pts['J']
-  print("nouvelle position : ", new_x, new_y)
-  print("on s'est déplacé de ", new_x-x, " mm en x et de ", new_y-y, " mm en y")
 
-  err_pos = np.array([new_x - (x + dstep_x), new_y - (y + dstep_y)])
-  print("erreur de position en x : ", err_pos[0] * 1000, " mm")
-  print("erreur de position en y : ", err_pos[1] * 1000, " mm")  
-  err_rel = np.array([None, None])
-  if dstep_x != 0 : err_rel[0] = err_pos[0] / dstep_x
-  if dstep_y != 0 : err_rel[1] = err_pos[1] / dstep_y
-  if dstep_x != 0 : print("erreur relative en x : ", err_rel[0])
-  if dstep_y != 0 : print("erreur relative en y : ", err_rel[1])
-  print("\n")
+    pts = k.get_leg_points_V1_V2(v1 / 1000, v2 / 1000, k.LEGS[k.FR]['lengths'])
+    x, y = pts['J'][0], pts['J'][1] 
+    Jacob = gen_jacob(pts, v1 / 1000, v2 / 1000)
 
-  return err_rel
+    Dpos = np.array([dstep_x / 1000, dstep_y / 1000])
+    DV = np.dot(Jacob, Dpos)
+
+    pts = k.get_leg_points_V1_V2(v1 / 1000 + DV[0], v2 / 1000 + DV[1], k.LEGS[k.FR]['lengths'])
+    print("position initiale : ", x * 1000, y * 1000)
+
+    new_x, new_y = pts['J']
+    print("nouvelle position : ", new_x * 1000, new_y * 1000)
+    print("on s'est déplacé de ", (new_x - x) * 1000, " mm en x et de ", (new_y - y) * 1000,
+          " mm en y")
+
+    err_pos = np.array([new_x - (x + dstep_x/1000), new_y - (y + dstep_y/1000)])
+    print("erreur de position en x : ", err_pos[0] * 1000, " mm")
+    print("erreur de position en y : ", err_pos[1] * 1000, " mm")
+    err_rel = np.array([None, None])
+    if dstep_x != 0: err_rel[0] = err_pos[0]*1000 / dstep_x
+    if dstep_y != 0: err_rel[1] = err_pos[1]*1000 / dstep_y
+    if dstep_x != 0: print("erreur relative en x : ", err_rel[0])
+    if dstep_y != 0: print("erreur relative en y : ", err_rel[1])
+    print("\n")
+
+    return err_rel
+
 
 '''
 Trace la trajectoire de la patte du robot entre 2 points de l'espace en suivant move_xyz
 '''
+
+
 def test_move_xyz(x0, y0, z0, x, y, z, dstep, eps):
-  # Tableau pour les 3 axes
-  X = [x0, x]
-  Y = [y0, y]
-  Z = [z0, z]
+    # Théorique
+    Xt = [x0, x]
+    Yt = [y0, y]
+    Zt = [z0, z]
 
-  # Tracé du résultat en 3D
-  fig = plt.figure()
-  ax = fig.gca(projection='3d')  # Affichage en 3D
-  ax.plot(X, Y, Z, label='Courbe')  # Tracé de la courbe 3D
-  plt.title("Courbe 3D")
-  ax.set_xlabel('X')
-  ax.set_ylabel('Y')
-  ax.set_zlabel('Z')
-  plt.tight_layout()
-  plt.show()
+    #Positions des vérins
+    L = move_xyz(x, y, z, dstep, eps)
+    print (V, L[0])
+    Pos = list(map(direct_xyz, [v[0] for v in L], [v[1] for v in L], [v[2] for v in L]))
+    print((x0, y0, z0), Pos[0])
+    X = [p[0] for p in Pos]
+    Y = [p[1] for p in Pos]
+    Z = [p[2] for p in Pos]
 
+    # Tracé du résultat en 3D
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')  # Affichage en 3D
+    ax.plot(Xt, Yt, Zt, label='Théorique')  # Tracé de la courbe 3D
+    ax.scatter(X, Y, Z, label='Positions',
+               marker='d')  # Tracé des points 3D
+    plt.title("Trajectoire patte FR")
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.tight_layout()
+    plt.show()
 
 
 ################################# TESTS ####################################
 
+t_move = 1
+# test de move_xyz
+if t_move:
+    x0, y0, z0 = direct_xyz(V[0], V[1], V[2])
+    #print(x0, y0, z0)
+    test_move_xyz(x0, y0, z0, 750, -600, -550, 10, 10)
+
 t_jacob = 0
 # test de l'erreur relative de position en appliquant la Jacobienne
-if t_jacob == 1:
-  test_jacob(495/1000, 585/1000, 10/1000, 0)
-  test_jacob(505/1000, 585/1000, 10/1000, 0)
-  test_jacob(515/1000, 585/1000, 10/1000, 0)
+if t_jacob:
+    test_jacob(495, 585, 10, 0)
+    test_jacob(505, 585, 10, 0)
+    test_jacob(515, 585, 10, 0)
 
-  test_jacob(495/1000, 585/1000, -10/1000, 0)
-  test_jacob(505/1000, 585/1000, -10/1000, 0)
-  test_jacob(515/1000, 585/1000, -10/1000, 0)
+    test_jacob(495, 585, -10, 0)
+    test_jacob(505, 585, -10, 0)
+    test_jacob(515, 585, -10, 0)
 
-  test_jacob(495/1000, 585/1000, 0, 10/1000)
-  test_jacob(505/1000, 585/1000, 0, 10/1000)
-  test_jacob(515/1000, 585/1000, 0, 10/1000)
+    test_jacob(495, 585, 0, 10)
+    test_jacob(505, 585, 0, 10)
+    test_jacob(515, 585, 0, 10)
 
-  test_jacob(495/1000, 585/1000, 0, -10/1000)
-  test_jacob(505/1000, 585/1000, 0, -10/1000)
-  test_jacob(515/1000, 585/1000, 0, -10/1000)
+    test_jacob(495, 585, 0, -10)
+    test_jacob(505, 585, 0, -10)
+    test_jacob(515, 585, 0, -10)
 
-  test_jacob(495/1000, 585/1000, 10/1000, 10/1000)
-  test_jacob(505/1000, 585/1000, 10/1000, 10/1000)
-  test_jacob(515/1000, 585/1000, 10/1000, 10/1000)
+    test_jacob(495, 585, 10, 10)
+    test_jacob(505, 585, 10, 10)
+    test_jacob(515, 585, 10, 10)
 
-  test_jacob(495/1000, 585/1000, 10/1000, -10/1000)
-  test_jacob(505/1000, 585/1000, 10/1000, -10/1000)
-  test_jacob(515/1000, 585/1000, 10/1000, -10/1000)
+    test_jacob(495, 585, 10, -10)
+    test_jacob(505, 585, 10, -10)
+    test_jacob(515, 585, 10, -10)
 
-  test_jacob(495/1000, 585/1000, -10/1000, 10/1000)
-  test_jacob(505/1000, 585/1000, -10/1000, 10/1000)
-  test_jacob(515/1000, 585/1000, -10/1000, 10/1000)
+    test_jacob(495, 585, -10, 10)
+    test_jacob(505, 585, -10, 10)
+    test_jacob(515, 585, -10, 10)
 
-  test_jacob(495/1000, 585/1000, -10/1000, -10/1000)
-  test_jacob(505/1000, 585/1000, -10/1000, -10/1000)
-  test_jacob(515/1000, 585/1000, -10/1000, -10/1000)
+    test_jacob(495, 585, -10, -10)
+    test_jacob(505, 585, -10, -10)
+    test_jacob(515, 585, -10, -10)
 
 ############################################################################
-
 
 
 ################################ DEPRECATED ################################
 
 t_comp = 0
 # comparaison en temps de nos modèles directs (v1 / Julien)
-if t_comp == 1:
-  t = time.time()
-  for i in range (10000):
-    direct_v1(495, 585)
-  t1 = time.time() - t
+if t_comp:
+    t = time.time()
+    for i in range(10000):
+        direct_v1(495, 585)
+    t1 = time.time() - t
 
-  t = time.time()
-  for i in range (10000):
-    k.get_leg_points_V1_V2(495/1000, 585/1000, k.LEG_PARTS_LENGTHS)['J']
-  t2 = time.time() - t
+    t = time.time()
+    for i in range(10000):
+        k.get_leg_points_V1_V2(495 / 1000, 585 / 1000 ,
+                               k.LEGS[k.FR]['lengths'])['J']
+    t2 = time.time() - t
 
-  print("direct_v1 prend ", t1*100, " us")
-  print("L'algo de Julien prend ", t2*100, " us")
+    print("direct_v1 prend ", t1 * 100, " us")
+    print("L'algo de Julien prend ", t2 * 100, " us")
 
 ############################################################################
